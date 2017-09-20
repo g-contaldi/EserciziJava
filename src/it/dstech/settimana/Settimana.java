bfc4070622cc6d1042b28126db4fd2c4c93dac5e
gianluca <g.contaldi@dstech.it>
2017-09-20 10:52:48 +0200

    mod settimana

--- src/it/dstech/settimana/Settimana.java
+++ src/it/dstech/settimana/Settimana.java
@@ -4,59 +4,52 @@ import java.util.Scanner;
 
 public class Settimana {
 
-	private static Scanner input;
+	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		init();
+		stampaGiorno();
 	}
 
-	private static void init() {
-		input = new Scanner(System.in);
+	private static void stampaGiorno() {
 		int giorno = 0;
-
+		boolean exit = false;
 		do {
-
 			System.out.println(
 					"Inserisci numero da 1 a 7 per stampare il nome del giorno per esteso, oppure 8 per terminare:");
 			giorno = input.nextInt();
 			input.nextLine();
-			stampaGiorno(giorno);
-		} while (true);
-	}
 
-	public static void stampaGiorno(int giorno) {
+			switch (giorno) {
 
-		switch (giorno) {
-
-		case 1:
-			System.out.println("Lunedi");
-			break;
-		case 2:
-			System.out.println("Martedi");
-			break;
-		case 3:
-			System.out.println("Mercoledi");
-			break;
-		case 4:
-			System.out.println("Giovedi");
-			break;
-		case 5:
-			System.out.println("Venerdi");
-			break;
-		case 6:
-			System.out.println("Sabato");
-			break;
-		case 7:
-			System.out.println("Domenica");
-			break;
-		case 8:
-			System.out.println("Grazie per l'utilizzo");
-			input.close();
-			System.exit(0);
-		default:
-			init();
-		}
-
+			case 1:
+				System.out.println("Lunedi");
+				break;
+			case 2:
+				System.out.println("Martedi");
+				break;
+			case 3:
+				System.out.println("Mercoledi");
+				break;
+			case 4:
+				System.out.println("Giovedi");
+				break;
+			case 5:
+				System.out.println("Venerdi");
+				break;
+			case 6:
+				System.out.println("Sabato");
+				break;
+			case 7:
+				System.out.println("Domenica");
+				break;
+			case 8:
+				System.out.println("Grazie per l'utilizzo");
+				exit = true;
+				break;
+			default:
+				stampaGiorno();
+			}
+		} while (!exit);
 	}
 }
