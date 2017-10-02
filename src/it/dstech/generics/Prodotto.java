81642891e9a7e05509731646181942d0ba91fb5c
gianluca <g.contaldi@dstech.it>
2017-10-02 11:57:45 +0200

    added example generics

---
+++ src/it/dstech/generics/Prodotto.java
@@ -0,0 +1,25 @@
+package it.dstech.generics;
+
+public class Prodotto<T> {
+
+	private T prodotto;
+
+	public Prodotto(T prodotto) {
+		super();
+		this.prodotto = prodotto;
+	}
+
+	public T getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(T prodotto) {
+		this.prodotto = prodotto;
+	}
+
+	@Override
+	public String toString() {
+		return "" + prodotto + "";
+	}
+
+}
